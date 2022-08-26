2b9bd1f162377cc401aba82bbb74505ab4b5daa2
mrvnjr <marvin.magboojr@g.batstate-u.edu.ph>
2022-08-26 12:48:49 +0800

    first commit

---
+++ html/admin_sidebar.php
@@ -0,0 +1,18 @@
+
+    <div class="dash-nav dash-nav-light ">
+            <header class="bg-success">
+                <a href="#!" class="menu-toggle">
+                    <i class="fas fa-bars"></i>
+                </a>
+            </header>
+            <nav class="dash-nav-list">
+                <a href="index.html" class="dash-nav-item"><i class="fas fa-home"></i> Dashboard </a>  
+                <a href="subjects.php"class="dash-nav-item"><i class="fas fa-book"></i>Subject</a>
+                <a href="class.php"class="dash-nav-item"><i class="fas fa-user-friends"></i> Class</a>
+                <a href="admin_user.php"class="dash-nav-item"><i class="fas fa-user-friends"></i>Admin Users</a>
+                <a href="students.php"class="dash-nav-item"><i class="fas fa-user-friends"></i>Students</a>
+                <a href="teachers.php"class="dash-nav-item"><i class="fas fa-user-friends"></i>Teachers</a>
+                <a href="school_year.php"class="dash-nav-item"><i class="fas fa-user-friends"></i>School Year</a>
+            </nav>
+
+</div>
