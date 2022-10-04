289f279df2964115c4c41c10775a2621829e70ee
mrvnjr <marvin.magboojr@g.batstate-u.edu.ph>
2022-10-04 12:06:01 +0800

    logout final

--- navbar.php
+++ navbar.php
@@ -10,7 +10,7 @@
     </a>
     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu1">
         <a class="dropdown-item" href="#!">Profile</a>
-        <a class="dropdown-item" href="login.html">Logout</a>
+        <a class="dropdown-item" href="logout.php">Logout</a>
     </div>
 </div>
 <!-- </header> -->
