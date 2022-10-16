9188f3b73596294723e61e6fbb2915c1341fbcfc
mrvnjr <marvin.magboojr@g.batstate-u.edu.ph>
2022-10-16 14:49:04 +0800

    student framework

---
+++ student/dashboard_student.php
@@ -0,0 +1,33 @@
+<?php include('header_dashboard.php'); ?>
+<!--?php include('session.php'); ?-->
+<body>
+<div class="dash">  
+	<?php include('student_sidebar.php'); ?>    
+    <div class="dash-app">
+            <header class="dash-toolbar ">
+                <a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a>
+            </header>
+            <main class="dash-content">
+				<h1>My Class</h1>
+                <div class="container-fluid">
+                    <div class="row">
+                    <div class="col-lg-12" id=" ">
+							<div class="card">
+								<div class="card-body">
+                                    dfdada
+									
+
+								</div>
+							</div>
+                        </div>
+
+                                                       
+                    </div>
+                </div>
+            </main> 	
+    </div>
+</div>
+<?php include('script.php'); ?>
+</body>
+
+<!-- ______________________break_______________________ -->
